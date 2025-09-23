91cf27ba9e46b9d251f736cbeb3f917352b850dd
Danilo <danilohernandesdesouza@gmail.com>
2025-09-23 18:09:57 -0300

    chore: persisting temperature data from amqp

--- microservices/temperature-monitoring/src/main/java/com/algaworks/algasensors/temperature/monitoring/infrasctructure/rabbitmq/RabbitMQListener.java
+++ microservices/temperature-monitoring/src/main/java/com/algaworks/algasensors/temperature/monitoring/infrasctructure/rabbitmq/RabbitMQListener.java
@@ -1,15 +1,12 @@
 package com.algaworks.algasensors.temperature.monitoring.infrasctructure.rabbitmq;
 
 import com.algaworks.algasensors.temperature.monitoring.api.model.TemperatureLogData;
-import io.hypersistence.tsid.TSID;
+import com.algaworks.algasensors.temperature.monitoring.domain.service.TemperatureMonitoringService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
-
-import java.util.Map;
 
 import static com.algaworks.algasensors.temperature.monitoring.infrasctructure.rabbitmq.RabbitMQConfig.PROCESS_TEMPERATURE_V_1_Q;
 
@@ -18,12 +15,10 @@ import static com.algaworks.algasensors.temperature.monitoring.infrasctructure.r
 @RequiredArgsConstructor
 public class RabbitMQListener {
 
+    private final TemperatureMonitoringService temperatureMonitoringService;
+
     @RabbitListener(queues = PROCESS_TEMPERATURE_V_1_Q)
-    public void handle(@Payload TemperatureLogData temperatureLogData,
-                       @Headers Map<String, Object> headers) {
-        TSID sensorId = temperatureLogData.getSensorId();
-        Double value = temperatureLogData.getValue();
-        log.info("Temperature updated: {} - {}", sensorId, value);
-        log.info("Headers: {}", headers);
+    public void handle(@Payload TemperatureLogData temperatureLogData) {
+        temperatureMonitoringService.processTemperatureReading(temperatureLogData);
     }
 }
